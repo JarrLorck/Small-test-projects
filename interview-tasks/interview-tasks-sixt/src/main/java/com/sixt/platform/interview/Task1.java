ea88e874026462a5ae1ed1d101585dae200b83da
Yauheni Baskin <y_baskin@indatalabs.com>
2018-08-02 11:30:11 +0300

    Comments were added

--- interview-tasks/interview-tasks-sixt/src/main/java/com/sixt/platform/interview/Task1.java
+++ interview-tasks/interview-tasks-sixt/src/main/java/com/sixt/platform/interview/Task1.java
@@ -5,12 +5,14 @@ import java.util.Arrays;
 public class Task1 {
 
     public boolean isAnagram(String lhs, String rhs) {
+        //I assume that empty strings are not anagrams and return false
         if (lhs == null || rhs == null || lhs.length() == 0 || rhs.length() == 0 || lhs.length() != rhs.length()) {
             return false;
         }
 
         char[] left = lhs.toCharArray();
         char[] right = rhs.toCharArray();
+        //Use common Java libs to sort arrays
         Arrays.sort(left);
         Arrays.sort(right);
 
