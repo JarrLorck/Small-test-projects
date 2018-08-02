d88208e4e970c982c6926025e55168630130fabd
Yauheni Baskin <y_baskin@indatalabs.com>
2018-08-02 11:00:16 +0300

    Task1 solved

---
+++ interview-tasks/interview-tasks-sixt/src/main/java/com/sixt/platform/interview/Task1.java
@@ -0,0 +1,31 @@
+package com.sixt.platform.interview;
+
+import java.util.Arrays;
+
+public class Task1 {
+
+    public boolean isAnagram(String lhs, String rhs) {
+        if (lhs == null || rhs == null || lhs.length() == 0 || rhs.length() == 0 || lhs.length() != rhs.length()) {
+            return false;
+        }
+
+        char[] left = lhs.toCharArray();
+        char[] right = rhs.toCharArray();
+        Arrays.sort(left);
+        Arrays.sort(right);
+
+        for (int i = 0; i < right.length; i++) {
+            if (left[i] != right[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Task1 task1 = new Task1();
+        boolean anagram = task1.isAnagram("anagramw", "nagaramt");
+        System.out.println(anagram);
+    }
+}
